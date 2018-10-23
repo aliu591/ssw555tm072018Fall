3aec76e7eb7290ff16978294d3435b6175ad7288
Ao Liu <591577589@qq.com>
2018-10-23 16:52:18 -0400

    sprint2 change

--- CheckDate.java
+++ CheckDate.java
@@ -1,3 +1,4 @@
+package sprint2;
 import java.io.IOException;
 import java.time.LocalDate;
 import java.util.*;
@@ -223,5 +224,39 @@ public class CheckDate {
             }
         }
     }
+
+     /**
+     * sprint2 US15 There should be fewer than 15 siblings in a family
+     */
+	public void US15(List<Family> families) {
+        Family fam;
+        for (int i = 0; i < families.size(); i++) {
+               fam = families.get(i);
+               if(fam.id_children.size()>15){
+                    System.out.println("ERROR: FAMILY: US15: "+fam.id_fam+ " has more than 15 siblings");
+               }
+        }
+    }
+    
+   
+    /**
+        * sprint2 US22 All individual IDs should be unique and all family IDs should be unique
+     */
+   public void US22(List<Person> people, List<Family> families) {
+       Set<String> person = new HashSet<>();
+        for (Person name : people) {
+            if (person.add(name.id_indi) == false) {
+                System.out.println("ERROR: INDIVIDUAL: US22: individual ID " + name.id_indi + " should be unique.");
+            }
+        }
+        Set<String> family = new HashSet<>();
+        for(Family fam: families){
+            if(family.add(fam.id_fam) == false){
+                System.out.println("ERROR: Family: US22: family ID " + fam.id_fam + " should be unique.");
+            }
+        }
+
+   }
+
 
 }
